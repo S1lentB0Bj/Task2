f3defd34e8582caee5970651bdb257206b4cae33
S1lentB0Bj <89969631+S1lentB0Bj@users.noreply.github.com>
2021-09-02 12:30:56 +0300

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <math.h>
+
+class PowClass
+{
+public:
+	float R = 2.6, S = 3.15;
+
+	void setR(float r)
+	{
+		R = r;
+	};
+	void setS(float s)
+	{
+		S = s;
+	};
+
+	float calculate()
+	{
+		return pow(R, S);
+	};
+
+};
+int main()
+{
+	PowClass a;
+	a.setR(3.14);
+	a.setS(5.4);
+	a.calculate();
+
+	std::cout << "Res: " << a.calculate();
+	return 0;
+
+};
